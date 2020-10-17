8b995fc0ae69ab32b85ced0c52d1e0222b92e127
Jesse Parent <jesseparent@gmail.com>
2020-10-16 21:49:49 -0600

    using object as connection information

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const database = 'ice_creamDB';
+const database = 'ice_creamDB'// Create the connection to database
 
-// Create the connection to database
-const connection = mysql.createConnection({
+const dbConn = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: database
-});
+};
+
+const connection = mysql.createConnection(dbConn);
 
 connection.connect(err => {
   if (err) throw err;
@@ -34,5 +35,7 @@ afterConnection = () => {
     connection.end();
   });
   //
-  
-};
+
+
+  connection.end();
+}
