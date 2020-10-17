4e4cb19b2d2fb6ea0e189195f9fc9b9cb69dbdb2
Jesse Parent <jesseparent@gmail.com>
2020-10-17 12:18:24 -0600

    incorporated department choice in inquirer

--- utils/viewHandler.js
+++ utils/viewHandler.js
@@ -1,9 +1,10 @@
 const cTable = require('console.table');
+const inquirer = require('inquirer');
 const db = require('../db/database');
 
 // Handle all Read/View operations of our CRUD
 const viewHandler = (choice, nextAction) => {
-  let sqlQuery = '';
+  let sqlQuery = null;
 
   // Generate the SQL Query based on the user choice
   switch (choice) {
@@ -24,16 +25,52 @@ const viewHandler = (choice, nextAction) => {
       JOIN department d ON d.id = r.department_id
       ORDER BY e.id ASC`;
       break;
+    case 'View total utilized budget by department':
+      viewBudgets(nextAction);
+      break;
     default:
       console.log('Unrecognized Option!');
       nextAction();
   }
 
   // Execute the SQL Query
-  db.query(sqlQuery, function (err, result) {
+  if (sqlQuery) {
+    db.query(sqlQuery, function (err, result) {
+      if (err) throw err;
+      console.table(result);
+      nextAction();
+    });
+  }
+};
+
+const viewBudgets = (nextAction) => {
+  // Get the Department list
+  db.query('SELECT name FROM department', function (err, result) {
     if (err) throw err;
-    console.table(result);
-    nextAction();
+
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'deptChoice',
+        message: 'Which department would you like to view the budget for?',
+        choices: result
+      }
+    ])
+      .then(dept => {
+        const sqlQuery = `SELECT d.name AS department, SUM(r.salary) AS "total utilized budget"
+        FROM department d
+        JOIN role r ON r.department_id = d.id
+        WHERE name = "${dept.deptChoice}"
+        GROUP BY d.name
+        ORDER BY 2 ASC;`;
+
+        //Execute the SQL Query
+        db.query(sqlQuery, function (err, result) {
+          if (err) throw err;
+          console.table(result);
+          nextAction();
+        });
+      });
   });
 };
 
