b9cb513a06650de984336e20cd56c72c7202bcd5
Jesse Parent <jesseparent@gmail.com>
2020-10-17 16:05:09 -0600

    Bonus views achieved

--- utils/viewHandler.js
+++ utils/viewHandler.js
@@ -18,12 +18,18 @@ const viewHandler = (choice, nextAction) => {
       ORDER BY r.id ASC;`;
       break;
     case 'View all employees':
-      sqlQuery = `SELECT e.id, e.first_name, e.last_name, r.title, d.name, r.salary, CONCAT(e2.first_name, " ", e2.last_name) AS manager
+      sqlQuery = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(e2.first_name, " ", e2.last_name) AS manager
       FROM employee e
       LEFT JOIN employee e2 ON e2.id = e.manager_id
       JOIN role r ON r.id = e.role_id
       JOIN department d ON d.id = r.department_id
       ORDER BY e.id ASC`;
+      break;
+    case 'View all employees by manager':
+      viewEmployeesByManager(nextAction);
+      break;
+    case 'View all employees by department':
+      viewEmployeesByDepartment(nextAction);
       break;
     case 'View total utilized budget by department':
       viewBudgets(nextAction);
@@ -43,6 +49,78 @@ const viewHandler = (choice, nextAction) => {
   }
 };
 
+const viewEmployeesByManager = (nextAction) => {
+  // Get the Employee list
+  db.query('SELECT CONCAT(first_name, " ", last_name) AS name FROM employee', function (err, result) {
+    if (err) throw err;
+
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'name',
+        message: 'Which manager would you like to view the employees for?',
+        choices: result
+      }
+    ])
+      .then(manager => {
+        const [firstName, lastName] = manager.name.split(' ');
+        const nameColumns = [{
+          first_name: firstName
+        },
+        {
+          last_name: lastName
+        }];
+        const sqlQuery = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(e2.first_name, " ", e2.last_name) AS manager
+        FROM employee e
+        LEFT JOIN employee e2 ON e2.id = e.manager_id
+        JOIN role r ON r.id = e.role_id
+        JOIN department d ON d.id = r.department_id
+        WHERE e.manager_id =
+        (SELECT id FROM employee WHERE ? AND ?) 
+        ORDER BY e.id ASC`;
+
+        //Execute the SQL Query
+        db.query(sqlQuery, nameColumns, function (err, result) {
+          if (err) throw err;
+          console.table(result);
+          nextAction();
+        });
+      });
+  });
+};
+
+const viewEmployeesByDepartment = (nextAction) => {
+  // Get the Department list
+  db.query('SELECT name FROM department', function (err, result) {
+    if (err) throw err;
+
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'name',
+        message: 'Which department would you like to view the employees for?',
+        choices: result
+      }
+    ])
+      .then(department => {
+        const sqlQuery = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(e2.first_name, " ", e2.last_name) AS manager
+        FROM employee e
+        LEFT JOIN employee e2 ON e2.id = e.manager_id
+        JOIN role r ON r.id = e.role_id
+        JOIN department d ON d.id = r.department_id
+        WHERE ?
+        ORDER BY e.id ASC`;
+
+        //Execute the SQL Query
+        db.query(sqlQuery, department, function (err, result) {
+          if (err) throw err;
+          console.table(result);
+          nextAction();
+        });
+      });
+  });
+};
+
 const viewBudgets = (nextAction) => {
   // Get the Department list
   db.query('SELECT name FROM department', function (err, result) {
@@ -51,21 +129,21 @@ const viewBudgets = (nextAction) => {
     inquirer.prompt([
       {
         type: 'list',
-        name: 'deptChoice',
+        name: 'name',
         message: 'Which department would you like to view the budget for?',
         choices: result
       }
     ])
-      .then(dept => {
+      .then(department => {
         const sqlQuery = `SELECT d.name AS department, SUM(r.salary) AS "total utilized budget"
         FROM department d
         JOIN role r ON r.department_id = d.id
-        WHERE name = "${dept.deptChoice}"
+        WHERE ?
         GROUP BY d.name
         ORDER BY 2 ASC;`;
 
         //Execute the SQL Query
-        db.query(sqlQuery, function (err, result) {
+        db.query(sqlQuery, department, function (err, result) {
           if (err) throw err;
           console.table(result);
           nextAction();
