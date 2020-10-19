dea9476969db8cf0bb3769f8275600d5cbe3d774
Jesse Parent <jesseparent@gmail.com>
2020-10-18 22:49:59 -0600

    removed a console log that wasn't needed

--- db/processQuery.js
+++ db/processQuery.js
@@ -3,7 +3,6 @@ const processQuery = (sqlQuery, nextAction, db, successMessage, keyValues = {}) 
   //Execute the SQL Query
   db.query(sqlQuery, keyValues, function (err, result) {
     if (err) {
-      console.log(err);
       throw err;
     }
     // Display the success message as feedback to the user
